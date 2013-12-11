cdd79e3f523241d7285f6a0e4837eb30854afbc4
Mike C <mcrosson_scm at nusku dot net>
2013-12-11 16:24:21 -0500

    Added missing styles that weren't initially imported during Locale API combine

--- src/com/dattasmoon/pebble/plugin/AbstractPluginActivity.java
+++ src/com/dattasmoon/pebble/plugin/AbstractPluginActivity.java
@@ -22,128 +22,117 @@ import android.view.MenuItem;
 import com.twofortyfouram.locale.BreadCrumber;
 
 public abstract class AbstractPluginActivity extends Activity {
-	public enum Mode {
-		STANDARD, LOCALE
-	};
+    public enum Mode {
+        STANDARD, LOCALE
+    };
 
-	private boolean mIsCancelled = false;
-	protected Mode mode = Mode.STANDARD;
-	Bundle localeBundle = null;
+    private boolean mIsCancelled = false;
+    protected Mode  mode         = Mode.STANDARD;
+    Bundle          localeBundle = null;
 
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-		localeBundle = getIntent().getBundleExtra(
-				com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        localeBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
 
-		if (getIntent().getAction() == "com.twofortyfouram.locale.intent.action.EDIT_SETTING") {
-			mode = Mode.LOCALE;
-			if (Constants.IS_LOGGABLE) {
-				Log.i(Constants.LOG_TAG, "Activity mode is set to locale");
-			}
-		} else {
-			mode = Mode.STANDARD;
-			if (Constants.IS_LOGGABLE) {
-				Log.i(Constants.LOG_TAG, "Activity mode is set to standard");
-			}
-		}
+        if (getIntent().getAction() == "com.twofortyfouram.locale.intent.action.EDIT_SETTING") {
+            mode = Mode.LOCALE;
+            if (Constants.IS_LOGGABLE) {
+                Log.i(Constants.LOG_TAG, "Activity mode is set to locale");
+            }
+        } else {
+            mode = Mode.STANDARD;
+            if (Constants.IS_LOGGABLE) {
+                Log.i(Constants.LOG_TAG, "Activity mode is set to standard");
+            }
+        }
 
-		if (mode == Mode.LOCALE) {
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-				setupTitleApi11();
-			} else {
-				setTitle(BreadCrumber.generateBreadcrumb(
-						getApplicationContext(), getIntent(),
-						getString(R.string.app_name)));
-			}
-		}
-	}
+        if (mode == Mode.LOCALE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                setupTitleApi11();
+            } else {
+                setTitle(BreadCrumber.generateBreadcrumb(getApplicationContext(), getIntent(),
+                        getString(R.string.app_name)));
+            }
+        }
+    }
 
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupTitleApi11() {
-		CharSequence callingApplicationLabel = null;
-		try {
-			callingApplicationLabel = getPackageManager().getApplicationLabel(
-					getPackageManager().getApplicationInfo(getCallingPackage(),
-							0));
-		} catch (final NameNotFoundException e) {
-			if (Constants.IS_LOGGABLE) {
-				Log.e(Constants.LOG_TAG, "Calling package couldn't be found", e);
-			}
-		}
-		if (null != callingApplicationLabel) {
-			setTitle(callingApplicationLabel);
-		}
-	}
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupTitleApi11() {
+        CharSequence callingApplicationLabel = null;
+        try {
+            callingApplicationLabel = getPackageManager().getApplicationLabel(
+                    getPackageManager().getApplicationInfo(getCallingPackage(), 0));
+        } catch (final NameNotFoundException e) {
+            if (Constants.IS_LOGGABLE) {
+                Log.e(Constants.LOG_TAG, "Calling package couldn't be found", e);
+            }
+        }
+        if (null != callingApplicationLabel) {
+            setTitle(callingApplicationLabel);
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(final Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		if (mode == Mode.LOCALE) {
-			getMenuInflater().inflate(
-					R.menu.twofortyfouram_locale_help_save_dontsave, menu);
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        if (mode == Mode.LOCALE) {
+            getMenuInflater().inflate(R.menu.twofortyfouram_locale_help_save_dontsave, menu);
 
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-				setupActionBarApi11();
-			}
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                setupActionBarApi11();
+            }
 
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-				setupActionBarApi14();
-			}
-			return true;
-		}
-		return false;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setupActionBarApi14();
+            }
+            return true;
+        }
+        return false;
+    }
 
-	}
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBarApi11() {
+        getActionBar().setSubtitle(
+                BreadCrumber.generateBreadcrumb(getApplicationContext(), getIntent(), getString(R.string.app_name)));
+    }
 
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBarApi11() {
-		getActionBar().setSubtitle(
-				BreadCrumber.generateBreadcrumb(getApplicationContext(),
-						getIntent(), getString(R.string.app_name)));
-	}
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setupActionBarApi14() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            getActionBar().setIcon(getPackageManager().getApplicationIcon(getCallingPackage()));
+        } catch (final NameNotFoundException e) {
+            if (Constants.IS_LOGGABLE) {
+                Log.w(Constants.LOG_TAG, "An error occurred loading the host's icon", e);
+            }
+        }
+    }
 
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	private void setupActionBarApi14() {
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		try {
-			getActionBar()
-					.setIcon(
-							getPackageManager().getApplicationIcon(
-									getCallingPackage()));
-		} catch (final NameNotFoundException e) {
-			if (Constants.IS_LOGGABLE) {
-				Log.w(Constants.LOG_TAG,
-						"An error occurred loading the host's icon", e);
-			}
-		}
-	}
+    @Override
+    public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
+        if (Constants.IS_LOGGABLE) {
+            Log.i(Constants.LOG_TAG, "Abstract plugin: Selected menu item id: " + String.valueOf(item.getItemId()));
+        }
+        final int id = item.getItemId();
 
-	@Override
-	public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
-		if (Constants.IS_LOGGABLE) {
-			Log.i(Constants.LOG_TAG, "Abstract plugin: Selected menu item id: "
-					+ String.valueOf(item.getItemId()));
-		}
-		final int id = item.getItemId();
+        if (android.R.id.home == id) {
+            finish();
+            return true;
+        } else if (R.id.twofortyfouram_locale_menu_dontsave == id) {
+            mIsCancelled = true;
+            finish();
+            return true;
+        } else if (R.id.twofortyfouram_locale_menu_save == id) {
+            finish();
+            return true;
+        }
 
-		if (android.R.id.home == id) {
-			finish();
-			return true;
-		} else if (R.id.twofortyfouram_locale_menu_dontsave == id) {
-			mIsCancelled = true;
-			finish();
-			return true;
-		} else if (R.id.twofortyfouram_locale_menu_save == id) {
-			finish();
-			return true;
-		}
+        return super.onOptionsItemSelected(item);
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
-
-	protected boolean isCanceled() {
-		return mIsCancelled;
-	}
+    protected boolean isCanceled() {
+        return mIsCancelled;
+    }
 }
